791e94c0030e7533a703c20c5a5a026fbe56d395
danielwalke <daniel-walke@t-online.de>
2025-10-26 05:56:16 +0100

    pinia store for cards

---
+++ frontend/src/stores/Field.js
@@ -0,0 +1,14 @@
+import cards from '../utils/init/Cards.js'
+import { defineStore } from 'pinia'
+
+export const useFieldStore = defineStore('field', {
+  state: () => ({ cards: cards}),
+  getters: {
+    getCards: (state) => state.cards,
+  },
+  actions: {
+    changeCardState(cardIdx, newState) {
+      this.cards[cardIdx].isActive = newState
+    },
+}
+})
---
+++ frontend/src/utils/init/Cards.js
@@ -0,0 +1,33 @@
+const titles = [
+  "Monkey D. Luffy",
+  "Roronoa Zoro",
+  "Nami",
+  "Usopp",
+  "Sanji",
+  "Tony Tony Chopper",
+  "Nico Robin",
+  "Franky",
+  "Brook",
+  "Jinbe",
+  "Portgas D. Ace",
+  "Gol D. Roger",
+  "Shanks",
+  "Buggy",
+  "Dracule Mihawk",
+  "Boa Hancock",
+  "Trafalgar D. Water Law",
+  "Eustass Kid",
+  "Donquixote Doflamingo",
+  "Crocodile",
+  "Marshall D. Teach",
+  "Sabo",
+  "Kaido",
+  "Big Mom"
+];
+
+export const cards = titles.map((title, index) => ({
+  id: index + 1,
+  title: title,
+  isActive: true,
+  isHidden: false,
+}));
