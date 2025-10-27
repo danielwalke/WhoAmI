fbcdbe31cfbc950b7b2668a476226950b2367dd9
danielwalke <daniel-walke@t-online.de>
2025-10-27 05:59:10 +0100

    poc room creation

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,28 @@
 from typing import Union
+from fastapi import FastAPI, APIRouter, Request
+from meta.RoomCreation import RoomCreation
 
-from fastapi import FastAPI
+app = FastAPI(redirect_slashes=False)
+app.state.rooms = dict()
 
-app = FastAPI()
+router = APIRouter(
+    tags=["whoami"],
+    redirect_slashes=False
+)
 
-
-@app.get("/")
+@router.get("/hello")
 def read_root():
     return {"Hello": "World"}
+
+@router.post("/create_room")
+def create_room(request: Request, body:RoomCreation):
+    ## First proof of concept and later password in hashed forms in sqllite db
+    if body.room_name in request.app.state.rooms:
+        return {"Error": "Room already exists"}
+    app.state.rooms[body.room_name] = body.room_password
+    return {"Success": f"Room {body.room_name} created"}
+
+app.include_router(
+    router,
+    prefix="/whoami"
+)
