786797b60b0d6a7786012c1a62b5e9f43e6ce432
danielwalke <daniel.walke@ovgu.de>
2025-11-10 09:02:53 +0100

    hard delete image files

--- backend/routers/ImageRouter.py
+++ backend/routers/ImageRouter.py
@@ -7,6 +7,7 @@ from database.database_classes.Room import Room
 from database.database_classes.Image import Image
 from sqlmodel import select
 from utils.RoomCheck import verify_room_auth
+from constants.Upload import UPLOAD_DIR
 
 router = APIRouter(
     tags=[SERVER_PREFIX],
@@ -28,5 +29,8 @@ def delete_image(image:Image, room:Room, session: SessionDep):
     if not fetched_image or fetched_image.room_id != fetched_room.id:
         raise HTTPException(status_code=404, detail="Image not found in the specified room")
     session.delete(fetched_image)
+    file_path = UPLOAD_DIR / fetched_image.id
+    if file_path.exists():
+        file_path.unlink()
     session.commit()
     return {"detail": "Image deleted successfully"}
