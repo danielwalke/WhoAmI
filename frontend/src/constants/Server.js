e8d51a8d10b4aff886e7bcf95a9a9a50bf3144bd
danielwalke <daniel.walke@ovgu.de>
2025-11-11 09:53:05 +0100

    updated Server url

--- frontend/src/constants/Server.js
+++ frontend/src/constants/Server.js
@@ -1,3 +1,4 @@
-export const SERVER_URL = 'http://localhost:8000'
+// export const SERVER_URL = 'http://localhost:8000'
+export const SERVER_URL = 'https://daniel-walke.com'
 export const SERVER_PREFIX = 'whoami'
 export const WEBSOCKET_URL = `ws://localhost:8000/${SERVER_PREFIX}/ws`
