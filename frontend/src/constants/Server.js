12b97f3398fdf85edf06bdd54e04dd2a9a8e8b72
danielwalke <daniel.walke@ovgu.de>
2025-11-11 10:57:10 +0100

    websocket url update based on my domain

--- frontend/src/constants/Server.js
+++ frontend/src/constants/Server.js
@@ -1,4 +1,5 @@
 // export const SERVER_URL = 'http://localhost:8000'
 export const SERVER_URL = 'https://daniel-walke.com'
 export const SERVER_PREFIX = 'whoami'
-export const WEBSOCKET_URL = `ws://localhost:8000/${SERVER_PREFIX}/ws`
+// export const WEBSOCKET_URL = `ws://localhost:8000/${SERVER_PREFIX}/ws`
+export const WEBSOCKET_URL = `ws://daniel-walke.com/${SERVER_PREFIX}/ws`
