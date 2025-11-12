4c1b8550b4ebdc3a6fb2dd01f88453bacac288dd
danielwalke <daniel.walke@ovgu.de>
2025-11-12 05:58:28 +0100

    dynamic server url for testing & prod

--- frontend/src/constants/Server.js
+++ frontend/src/constants/Server.js
@@ -1,7 +1,5 @@
 const isSecure = window.location.protocol === 'https:';
 const wsProtocol = isSecure ? 'wss' : 'ws';
-// export const SERVER_URL = 'http://localhost:8000'
-export const SERVER_URL = 'https://daniel-walke.com'
+export const SERVER_URL = window.location.hostname === 'localhost' ? 'http://localhost:8000' : 'https://daniel-walke.com'
 export const SERVER_PREFIX = 'whoami'
-// export const WEBSOCKET_URL = `ws://localhost:8000/${SERVER_PREFIX}/ws`
-export const WEBSOCKET_URL = `${wsProtocol}://daniel-walke.com/${SERVER_PREFIX}/ws`
+export const WEBSOCKET_URL = window.location.hostname === 'localhost' ? `ws://localhost:8000/${SERVER_PREFIX}/ws` : `${wsProtocol}://daniel-walke.com/${SERVER_PREFIX}/ws`
